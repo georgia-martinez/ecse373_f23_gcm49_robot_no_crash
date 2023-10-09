ffda445ebe40a5ba1c82918aac4bb48a06d5e4d9
Georgia <gcm49@case.edu>
2023-10-08 22:40:43 -0400

    Started smart teleoperation code

---
+++ robot_no_crash/src/smart_teleop.cpp
@@ -0,0 +1,64 @@
+#include "sensor_msgs/LaserScan.h"
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Vector3.h"
+
+ros::Publisher cmd_vel_pub;
+
+sensor_msgs::LaserScan lidar_msg;
+geometry_msgs::Twist des_vel_msg;
+geometry_msgs::Twist new_vel_msg;
+
+void lidarCallback(const sensor_msgs::LaserScan msg) {
+	lidar_msg = msg;
+}
+
+void velocityCallback(const geometry_msgs::Twist msg) {
+	
+	new_vel_msg = msg;
+	
+	float x_pos = msg.linear.x;
+	int RANGE = 20;
+	
+	// If the robot is moving forwards
+	if (x_pos > 0) {
+	
+		// Check the lasers in front of the robot
+		int middle_index = lidar_msg.ranges.size() / 2;
+		int start = middle_index - RANGE;
+		int end = middle_index + RANGE;
+		
+		float min_dist = lidar_msg.ranges[start]; // minimum distance from wall
+		
+		for (int i = start; i < end; i++) {
+
+			if (min_dist > lidar_msg.ranges[i]) {
+				min_dist = lidar_msg.ranges[i];
+			}
+		}
+		
+		// If wall is nearby, rotate robot to not face wall
+		if (min_dist < 1) {
+			new_vel_msg.linear.x = 0;
+			new_vel_msg.angular.z = 0.5;
+		} else {
+			new_vel_msg = msg;
+		}
+	}
+	
+	cmd_vel_pub.publish(new_vel_msg);
+}
+
+int main(int argc, char* argv[]) {
+	ros::init(argc, argv, "lab4");
+	
+	ros::NodeHandle n;
+  	cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+
+  	ros::Subscriber laser_sub = n.subscribe("laser1", 1000, lidarCallback);  	
+  	ros::Subscriber des_vel_sub = n.subscribe("des_vel", 1000, velocityCallback);
+  	
+  	ros::spin();
+
+	return 0;
+}
